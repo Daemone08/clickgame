d61bc0ffe8383109e54e4e1a539b24267720f117
Daemone <kylebehm@gmail.com>
2018-08-22 13:56:40 -0400

    basic-structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+
+  // DEFINE STATEFUL VARIABLES AND METHODS HERE
+  // DEFINE STATEFUL VARIABLES AND METHODS HERE
+  // DEFINE STATEFUL VARIABLES AND METHODS HERE
+  // DEFINE STATEFUL VARIABLES AND METHODS HERE
+  state = {
+
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
       </div>
     );
   }
