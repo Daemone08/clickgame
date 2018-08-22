2787489d99d2efde03d9503484e82b6466fdc456
Daemone <kylebehm@gmail.com>
2018-08-22 14:13:10 -0400

    app-grid-pseudo-methods

--- src/App.js
+++ src/App.js
@@ -7,13 +7,60 @@ class App extends Component {
   // DEFINE STATEFUL VARIABLES AND METHODS HERE
   // DEFINE STATEFUL VARIABLES AND METHODS HERE
   // DEFINE STATEFUL VARIABLES AND METHODS HERE
+
+  // STATEFUL VARIABLES
   state = {
 
+    // create an array to map click images
+    picArray: [],
+
+    // create a score counter
+    score: 0
+
   };
+
+  // STATEFUL METHODS ( change to arrow functions)
+
+  checkPic() {
+
+  }
+
+  scoreCount() {
+
+  }
+
+  mapToArray() {
+
+  }
+
+  resetGame() {
+
+  }
+
+  // Method for checking against array, adjusting score, mapping click pic to array, adjusting score  and refreshing images
+  onClick() {
+
+  }
+
   render() {
     return (
-      <div>
+      // wrapper
+      <div className="container">
+        <div className="row">
+          <div className="col">
 
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+
+          </div>
+        </div>
       </div>
     );
   }
